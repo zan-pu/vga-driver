aec4c80c7c56dcc437eb7db359b2ff133228a6b4
spencerwoo <spencerwoo98@foxmail.com>
2019-09-09 23:13:11 +0800

    init: vga

---
+++ vga-driver.srcs/sources_1/new/vga_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/09/09 23:12:08
+// Design Name: 
+// Module Name: vga_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vga_top(
+
+    );
+endmodule
