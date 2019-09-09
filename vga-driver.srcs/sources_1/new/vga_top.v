007aab4560e445fbb8f8b9c83f14f5fc32fc7485
spencerwoo <spencerwoo98@foxmail.com>
2019-09-10 00:16:34 +0800

    add: vga top module

--- vga-driver.srcs/sources_1/new/vga_top.v
+++ vga-driver.srcs/sources_1/new/vga_top.v
@@ -1,26 +1,93 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/09/09 23:12:08
-// Design Name: 
-// Module Name: vga_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`define RST_EN 0
 
+module vga_top #(
+           parameter H_DISPLAY      = 11'd1024,
+           parameter H_FRONT_PORCH  = 11'd24,
+           parameter H_SYNC_PULSE   = 11'd136,
+           parameter H_BACK_PORCH   = 11'd160,
+           parameter H_TOTAL        = 11'd1344,
 
-module vga_top(
+           parameter V_DISPLAY      = 10'd768,
+           parameter V_FRONT_PORCH  = 10'd3,
+           parameter V_SYNC_PULSE   = 10'd6,
+           parameter V_BACK_PORCH   = 10'd29,
+           parameter V_TOTAL        = 10'd806
+       )(
+           input  wire      clk,
+           input  wire      rstn,
 
-    );
+           output wire      hs,
+           output wire      vs,
+           output wire[3:0] red,
+           output wire[3:0] green,
+           output wire[3:0] blue
+       );
+
+/* RGB signals */
+reg[3:0] reg_red;
+reg[3:0] reg_green;
+reg[3:0] reg_blue;
+
+assign red = reg_red;
+assign green = reg_green;
+assign blue = reg_blue;
+
+/* Horizonal & Vertical refresh pulses */
+
+reg h_reg;
+reg v_reg;
+
+assign hs = h_reg;
+assign vs = v_reg;
+
+reg[10:0] h_count;
+reg[10:0] v_count;
+
+/* horizontal scan and vertical scan */
+
+always @ (posedge clk) begin
+    if (rstn == `RST_EN) begin
+        h_count <= 11'b0;
+        v_count <= 11'b0;
+    end
+    else begin
+        if (h_count == H_TOTAL - 1) begin
+            h_count <= 11'b0;
+            if (v_count == V_TOTAL - 1) begin
+                v_count <= 11'b0;
+            end
+            else begin
+                v_count <= v_count + 1'b1;
+            end
+        end
+        else begin
+            h_count <= h_count + 1'b1;
+        end
+    end
+end
+
+always @ (posedge clk) begin
+    if (h_count < H_SYNC_PULSE) begin
+        h_reg <= 1'b0;
+    end
+    else begin
+        h_reg <= 1'b1;
+    end
+end
+
+always @ (posedge clk) begin
+    if (v_count < V_SYNC_PULSE) begin
+        v_reg <= 1'b0;
+    end
+    else begin
+        v_reg <= 1'b1;
+    end
+end
+
+/* Control RGB */
+
+always @ (posedge clk) begin
+
+end
 endmodule
