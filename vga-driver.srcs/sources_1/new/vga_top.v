c4d36a24c3f79e42fdddecb3ed60107992fa533d
spencerwoo <spencerwoo98@foxmail.com>
2019-09-10 21:02:36 +0800

    update: vga driver

--- vga-driver.srcs/sources_1/new/vga_top.v
+++ vga-driver.srcs/sources_1/new/vga_top.v
@@ -1,5 +1,9 @@
 `timescale 1ns / 1ps
-`define RST_EN 1'b0
+`include "params.vh"
+
+/*
+ * Module: VGA top module
+ */
 
 module vga_top #(
            parameter H_DISPLAY      = 11'd1024,
@@ -17,12 +21,55 @@ module vga_top #(
            input  wire      clk,
            input  wire      rstn,
 
+           input wire wen,
+           input wire[31:0] wdata,
+           input wire[3:0] addr,
+           input wire rdata,
+
            output wire      hs,
            output wire      vs,
            output wire[3:0] red,
            output wire[3:0] green,
            output wire[3:0] blue
        );
+
+
+reg[31:0] data[15:0];
+
+assign rdata = data[addr];
+reg[3:0] p_x;
+reg[3:0] p_y;
+reg[31:0] player_x;
+
+always @ (posedge clk_vga) begin
+    if (rstn == `RST_EN) begin
+        data[0]  <= 32'h00000000;
+        data[1]  <= 32'h15545518;
+        data[2]  <= 32'h10040110;
+        data[3]  <= 32'h11555510;
+        data[4]  <= 32'h11100010;
+        data[5]  <= 32'h11155554;
+        data[6]  <= 32'h11000000;
+        data[7]  <= 32'h11555454;
+        data[8]  <= 32'h10000444;
+        data[9]  <= 32'h15554444;
+        data[10] <= 32'h01000444;
+        data[11] <= 32'h15555444;
+        data[12] <= 32'h10010044;
+        data[13] <= 32'h15515554;
+        data[14] <= 32'h30100000;
+        data[15] <= 32'h00000000;
+        p_x <= 4'b0000;
+        p_y <= 4'b0000;
+        player_x <= 32'h00000000;
+    end
+end
+
+always @ (posedge clk)  begin
+    if (wen) begin
+        data[addr] <= wdata;
+    end
+end
 
 // 65 MHz clock signal
 wire clk_vga;
@@ -34,8 +81,6 @@ clk_wiz u_clk_wiz (
             .reset(~rstn), // input reset
             // Clock in ports
             .clk_in1(clk));      // input clk_in1
-
-reg[11:0] control_reg = 12'b111111110000;
 
 /* RGB signals */
 reg[3:0] reg_red;
@@ -98,11 +143,194 @@ always @ (posedge clk_vga) begin
     end
 end
 
-/* Control RGB */
 
 always @ (posedge clk_vga) begin
-    reg_red <= control_reg[11:8];
-    reg_green <= control_reg[7:4];
-    reg_blue <= control_reg[3:0];
+    player_x <= data[addr];
+    p_x <= addr;
+    if (player_x[29:28] == 2'b11) begin
+        p_y <= 4'b1111;
+    end else if(player_x[27:26] == 2'b11) begin
+        p_y <= 4'b1110;
+    end
+    else if(player_x[25:24] == 2'b11) begin
+        p_y <= 4'b1101;
+    end
+    else if(player_x[23:22] == 2'b11) begin
+        p_y <= 4'b1100;
+    end
+    else if(player_x[21:20] == 2'b11) begin
+        p_y <= 4'b1011;
+    end
+    else if(player_x[19:18] == 2'b11) begin
+        p_y <= 4'b1010;
+    end
+    else if(player_x[17:16] == 2'b11) begin
+        p_y <= 4'b1001;
+    end
+    else if(player_x[15:14] == 2'b11) begin
+        p_y <= 4'b1000;
+    end
+    else if(player_x[13:12] == 2'b11) begin
+        p_y <= 4'b0111;
+    end
+    else if(player_x[11:10] == 2'b11) begin
+        p_y <= 4'b0110;
+    end
+    else if(player_x[9:8] == 2'b11) begin
+        p_y <= 4'b0101;
+    end
+    else if(player_x[7:6] == 2'b11) begin
+        p_y <= 4'b0100;
+    end
+    else if(player_x[5:4] == 2'b11) begin
+        p_y <= 4'b0011;
+    end
+    else if(player_x[3:2] == 2'b11) begin
+        p_y <= 4'b0010;
+    end
+    else begin
+        p_y <= 4'b0000;
+    end
 end
+
+
+
+/* Control RGB */
+
+reg[1:0] debug;
+
+always @ (posedge clk_vga) begin
+    /* Game board */
+    if (h_count < 1140 && h_count > 500 && v_count < 740 && v_count > 100) begin
+        if(h_count>p_x*40+460 && h_count<p_x*40+500&&v_count>740-p_y*40 && v_count<780-p_y*40 && v_count!=0)begin
+                reg_red   <= `RED_R;
+                reg_green <= `RED_G;
+                reg_blue  <= `RED_B;
+        end
+        else if(h_count>500 && h_count<540) begin
+            reg_red   <= `BLACK_R;
+            reg_green <= `BLACK_G;
+            reg_blue  <= `BLACK_B;
+        end
+        else if(h_count>540 && h_count<580) begin
+            if((v_count>100&&v_count<140) || (v_count>380&&v_count<420) || (v_count>580 && v_count<620) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+            else if (v_count>660 &&v_count<700) begin
+                reg_red   <= `GREEN_R;
+                reg_green <= `GREEN_G;
+                reg_blue  <= `GREEN_B;
+            end
+        end
+        else if(h_count>580 && h_count<620) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<340) || (v_count>380 && v_count<540) || (v_count>580 && v_count<620) || (v_count>660 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>620 && h_count<660) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<220) || (v_count>580 && v_count<620) || (v_count>660 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>660 && h_count<700) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<220) || (v_count>260 && v_count<300) || (v_count>340 && v_count<620) || (v_count>660 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>700 && h_count<740) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<220) || (v_count>260 && v_count<300) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>740 && h_count<780) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<220) || (v_count>260 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>780 && h_count<820) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<220) || (v_count>540 && v_count<580) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>820 && h_count<860) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<500) || (v_count>540 && v_count<580) || (v_count>620 && v_count<660) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>860 && h_count<900) begin
+            if((v_count>100&&v_count<140) || (v_count>460&&v_count<500) || (v_count>540 && v_count<580) || (v_count>620 && v_count<660) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>900 && h_count<940) begin
+            if((v_count>100&&v_count<220) || (v_count>260&&v_count<500) || (v_count>540 && v_count<580) || (v_count>620 && v_count<660) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>940 && h_count<980) begin
+            if((v_count>100&&v_count<140) || (v_count>540 && v_count<580) || (v_count>620 && v_count<660) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>980 && h_count<1020) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<380) || (v_count>420 && v_count<580) || (v_count>620 && v_count<660) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>1020 && h_count<1060) begin
+            if((v_count>100&&v_count<140) || (v_count>340&&v_count<380) || (v_count>700 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>1060 && h_count<1100) begin
+            if((v_count>100&&v_count<140) || (v_count>180&&v_count<300) || (v_count>340 && v_count<740)) begin
+                reg_red   <= `BLACK_R;
+                reg_green <= `BLACK_G;
+                reg_blue  <= `BLACK_B;
+            end
+        end
+        else if(h_count>1100 && h_count<1140) begin
+            reg_red   <= `BLACK_R;
+            reg_green <= `BLACK_G;
+            reg_blue  <= `BLACK_B;
+        end
+        else begin
+            reg_red   <= `WHITE_R;
+            reg_green <= `WHITE_G;
+            reg_blue  <= `WHITE_B;
+        end
+    end
+    else begin
+        reg_red   <= `RED_R;
+        reg_green <= `RED_G;
+        reg_blue  <= `RED_B;
+    end
+end
+/* Out of bounds */
 endmodule
